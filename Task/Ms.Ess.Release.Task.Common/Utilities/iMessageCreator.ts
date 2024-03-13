d5aae635eb461a1b05011d1393f8f04edda48442
Pooja Patidar <poojapatidar897@gmail.com>
2024-03-13 12:40:58 +0530

    removing files

--- Task/Ms.Ess.Release.Task.Common/Utilities/iMessageCreator.ts
+++
@@ -1,7 +0,0 @@
-import * as GatewayClient from '../GatewayApiSpec/api'
-
-export interface IMessageCreator{
-    
-    PopulateReleaseRequestMessage(containerSas:URL):Promise<GatewayClient.MSEssGatewayClientContractsReleaseRequestReleaseRequestMessage>
-    PopulateSessionRequestMessage():Promise<GatewayClient.MSEssGatewayClientContractsSessionRequestMessage>
-}
--- Task/Ms.Ess.Release.Task.Core/Managers/iAuthContextManager.ts
+++
@@ -1,8 +0,0 @@
-import { IConfig } from "../../Ms.Ess.Release.Task.Common/Configuration/iConfig" 
-
-export interface IAuthenticationManager{
-    
-    accessToken?: string
-    config?: IConfig
-    setAccessToken():Promise<string| undefined>
-}
